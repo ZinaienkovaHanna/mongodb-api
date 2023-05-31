a5a707e71cd895de96600e1ffa735bead2fdfd07
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2023-05-31 14:38:07 +0300

    create server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { connectToDb, getDb } = require('./db');
+
+const PORT = 3000;
+
+const app = express();
+
+connectToDb((err) => {
+    if (!err) {
+        app.listen(PORT, (err) => {
+            err ? console.log(err) : console.log(`Listening port ${PORT}`);
+        });
+        db = getDb();
+    } else {
+        console.log(`DB connection error: ${err}`);
+    }
+});
