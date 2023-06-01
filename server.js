0dfb61df45e3b298289d1a1e848d5a1793c90f81
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2023-06-01 16:01:56 +0300

    add post, delete, patch in server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const { connectToDb, getDb } = require('./db');
+// const db = require('./db');
 
 const PORT = 3000;
 
 const app = express();
+app.use(express.json()); // middleware express json
+
+let db;
 
 connectToDb((err) => {
     if (!err) {
@@ -13,5 +18,74 @@ connectToDb((err) => {
         db = getDb();
     } else {
         console.log(`DB connection error: ${err}`);
+    }
+});
+
+const handleError = (res, error) => {
+    res.status(500).json({ error });
+};
+
+app.get('/movies', (req, res) => {
+    const movies = [];
+
+    db.collection('movies')
+        .find()
+        .sort({ title: 1 })
+        .forEach((movie) => movies.push(movie))
+        .then(() => {
+            res.status(200).json(movies);
+        })
+        .catch(() => handleError(res, 'Something goes wrong...'));
+});
+
+// find one element
+app.get('/movies/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('movies')
+            .findOne({ _id: new ObjectId(req.params.id) })
+            .then((doc) => {
+                res.status(200).json(doc);
+            })
+            .catch(() => handleError(res, 'Something goes wrong...'));
+    } else {
+        handleError(res, 'Id wrong');
+    }
+});
+
+// delete one element
+app.delete('/movies/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('movies')
+            .deleteOne({ _id: new ObjectId(req.params.id) })
+            .then((result) => {
+                res.status(200).json(result);
+            })
+            .catch(() => handleError(res, 'Something goes wrong...'));
+    } else {
+        handleError(res, 'Id wrong');
+    }
+});
+
+// add one element
+app.post('/movies', (req, res) => {
+    db.collection('movies')
+        .insertOne(req.body)
+        .then((result) => {
+            res.status(201).json(result);
+        })
+        .catch(() => handleError(res, 'Something goes wrong...'));
+});
+
+// update or change one element
+app.patch('/movies/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('movies')
+            .updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body })
+            .then((result) => {
+                res.status(200).json(result);
+            })
+            .catch(() => handleError(res, 'Something goes wrong...'));
+    } else {
+        handleError(res, 'Id wrong');
     }
 });
