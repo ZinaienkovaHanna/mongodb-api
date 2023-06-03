b3fa4179fb8040ff1dfa1e7c10bba6a5fca7ff7d
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2023-06-03 11:41:10 +0300

    MongoDB Atlas

---
+++ routes/movie-routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const {
+    getMovies,
+    getMovie,
+    deleteMovie,
+    addMovie,
+    updateMovie,
+} = require('../controllers/movie-controller');
+
+const router = express.Router();
+
+const handleError = (res, error) => {
+    res.status(500).json({ error });
+};
+
+router.get('/movies', getMovies);
+router.get('/movies/:id', getMovie);
+router.delete('/movies/:id', deleteMovie);
+router.post('/movies', addMovie);
+router.patch('/movies/:id', updateMovie);
+
+module.exports = router;
